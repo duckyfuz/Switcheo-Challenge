a74cb4850fc4b21dfbf422013af8b1baa4a6a76e
duckyfuz <kenf@iMac&cheese.local>
2023-08-21 22:57:47 +0800

    Replace Problem 5

--- Test5/migrations/1_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const TokenBalanceReader = artifacts.require("TokenBalanceReader");
-const MyToken = artifacts.require("MyToken");
-
-module.exports = function (deployer) {
-  deployer.deploy(TokenBalanceReader);
-  deployer.deploy(MyToken);
-};
